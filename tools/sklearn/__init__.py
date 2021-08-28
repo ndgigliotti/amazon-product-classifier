21f6a160e6cbf772feb6ab835ba591317399a6ed
ndgigliotti <ndgigliotti@gmail.com>
2021-08-28 19:29:04 -0400

    remove classification module

--- tools/sklearn/__init__.py
+++ tools/sklearn/__init__.py
@@ -1,2 +1,2 @@
 """Tools related to Scikit-Learn API. Should be renamed `sklearn`."""
-from . import classification, selection, transformers, vectorizers, wrappers
+from . import selection, transformers, vectorizers, wrappers
